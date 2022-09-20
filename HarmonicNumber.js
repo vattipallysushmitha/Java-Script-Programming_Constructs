d0baafc2a2779d7e2da84f48898556219349dd1f
sushmitha <vengalreddysushmitha@gmail.com>
2022-09-20 15:54:47 -0230

    [SUSHMITHA]ADDED:Selection pratice Problems with if and else problems are added

---
+++ HarmonicNumber.js
@@ -0,0 +1,12 @@
+/*
+Write a program that takes a command-line argument n and prints the nth harmonic
+number. Harmonic Number is of the form
+*/
+const prompt =require("prompt-sync")({ sigint: true});
+let harmonicNumber = parseInt(prompt("Enter the number to print harmonic number upto -> "));
+var singleLine = "";
+for(var i = 1 ; i <= harmonicNumber ;i++)
+{
+   singleLine += "1/"+i+" + ";
+}
+console.log(singleLine);
